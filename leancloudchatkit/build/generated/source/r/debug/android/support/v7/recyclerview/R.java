c1b0ac17a103862b855a245dbf18e489cfa6a1f0
guohouxiao <122492508@qq.com>
2017-10-28 18:27:00 +0800

    Update Android Studio

--- leancloudchatkit/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ leancloudchatkit/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -8,23 +8,23 @@ package android.support.v7.recyclerview;
 
 public final class R {
     public static final class attr {
-        public static int layoutManager = 0x7f0100a9;
-        public static int reverseLayout = 0x7f0100ab;
-        public static int spanCount = 0x7f0100aa;
-        public static int stackFromEnd = 0x7f0100ac;
+        public static int layoutManager = 0x7f040071;
+        public static int reverseLayout = 0x7f040097;
+        public static int spanCount = 0x7f0400a2;
+        public static int stackFromEnd = 0x7f0400a8;
     }
     public static final class dimen {
-        public static int item_touch_helper_max_drag_scroll_per_frame = 0x7f070048;
+        public static int item_touch_helper_max_drag_scroll_per_frame = 0x7f080049;
     }
     public static final class id {
-        public static int item_touch_helper_previous_elevation = 0x7f0b0005;
+        public static int item_touch_helper_previous_elevation = 0x7f0c005a;
     }
     public static final class styleable {
-        public static int[] RecyclerView = { 0x010100c4, 0x7f0100a9, 0x7f0100aa, 0x7f0100ab, 0x7f0100ac };
+        public static int[] RecyclerView = { 0x010100c4, 0x7f040071, 0x7f040097, 0x7f0400a2, 0x7f0400a8 };
         public static int RecyclerView_android_orientation = 0;
         public static int RecyclerView_layoutManager = 1;
-        public static int RecyclerView_reverseLayout = 3;
-        public static int RecyclerView_spanCount = 2;
+        public static int RecyclerView_reverseLayout = 2;
+        public static int RecyclerView_spanCount = 3;
         public static int RecyclerView_stackFromEnd = 4;
     }
 }
